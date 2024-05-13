928b16361ee22910c24104d08d023fd4ed2ba5fa
Brian Shimkus <brianshimkus1@gmail.com>
2024-05-13 15:56:03 -0500

    Manage subscription functionality

--- app/dashboard/billing/page.tsx
+++ app/dashboard/billing/page.tsx
@@ -1,11 +1,20 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-import { Card, CardContent } from '@/components/ui/card'
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card'
 import { CheckCircle2 } from 'lucide-react'
 import prisma from '@/app/lib/db'
 import { unstable_noStore as noStore } from 'next/cache'
-import { getStripeSession } from '@/app/lib/stripe'
+import { getStripeSession, stripe } from '@/app/lib/stripe'
 import { redirect } from 'next/navigation'
-import { StripeSubscriptionCreationButton } from '@/app/components/SubmitButtons'
+import {
+	StripePortal,
+	StripeSubscriptionCreationButton,
+} from '@/app/components/SubmitButtons'
 
 const featureItems = [
 	{ name: 'Lorem Ipsum something' },
@@ -65,6 +74,50 @@ export default async function BillingPage() {
 		})
 
 		return redirect(subscriptionUrl)
+	}
+
+	async function createCustomerPortal() {
+		'use server'
+		const session = await stripe.billingPortal.sessions.create({
+			customer: data?.user.stripeCustomerId as string,
+			return_url:
+				process.env.NODE_ENV === 'production'
+					? (process.env.PRODUCTION_URL as string)
+					: 'http://localhost:3000/dashboard',
+		})
+
+		return redirect(session.url)
+	}
+
+	if (data?.status === 'active') {
+		return (
+			<div className='grid items-start gap-8'>
+				<div className='flex items-center justify-between px-2'>
+					<div className='grid gap-1'>
+						<h1 className='text-3xl md:text-4xl '>Subscription</h1>
+						<p className='text-lg text-muted-foreground'>
+							Settings reagding your subscription
+						</p>
+					</div>
+				</div>
+
+				<Card className='w-full lg:w-2/3'>
+					<CardHeader>
+						<CardTitle>Edit Subscription</CardTitle>
+						<CardDescription>
+							Click on the button below, this will give you the opportunity to
+							change your payment details and view your statement at the same
+							time.
+						</CardDescription>
+					</CardHeader>
+					<CardContent>
+						<form action={createCustomerPortal}>
+							<StripePortal />
+						</form>
+					</CardContent>
+				</Card>
+			</div>
+		)
 	}
 
 	return (
